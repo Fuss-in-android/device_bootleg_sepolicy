4c397cd6775fc2a75a24b6ac9d42f91f16ea8580
Aayush Gupta <aayushgupta219@gmail.com>
2020-10-22 13:02:38 +0530

    sepolicy: Switch to BOARD_VENDOR_SEPOLICY_DIRS
    - BOARD_SEPOLICY_DIRS is deprecated and gives compile-time
      errors when used in unison with a device using BOARD_VENDOR_SEPOLICY_DIRS
    
    Ref:
    [0]: https://github.com/LineageOS/android_system_sepolicy/commit/ec3ac470a98342c13c1fec8d46433c73b08531be
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Icefb062cc8cdef532b4310684d9a66afe97e49c4

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -16,7 +16,7 @@ ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/bootleg/sepolicy/qcom/dynamic
 else
-BOARD_SEPOLICY_DIRS += \
+BOARD_VENDOR_SEPOLICY_DIRS += \
     device/bootleg/sepolicy/qcom/dynamic \
     device/bootleg/sepolicy/qcom/vendor
 endif
